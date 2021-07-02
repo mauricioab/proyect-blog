f6b3b920f572de4adbb3ec1cc4c382598fb8b686
mauricio <mauricio.firux@gmail.com>
2021-07-02 16:08:42 -0300

    kdfjkladjfkla

--- BLOG/index.php
+++ BLOG/index.php
@@ -8,7 +8,7 @@
 
             <article class="entrada">   
                 <a href="">
-                <h2>Probando</h2>
+                <h2>Probando2</h2>
                 <p>Descripción de la entrada(Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Maiores ex vero autem asperiores similique exercitationem numquam explicabo, mollitia
                 deleniti nisi a? Praesentium, totam fuga minus dicta sit quisquam blanditiis illum.
